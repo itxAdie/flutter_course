7923ba4907ae87e9588f16c9567f10f9ec888fe0
ItxAdie <itxadiert@gmail.com>
2025-05-22 21:35:04 +0500

    deleting file and adding data in files in dart

---
+++ file_writing_in_dart/test/file_writing_in_dart_test.dart
@@ -0,0 +1,8 @@
+// import 'package:file_writing_in_dart/file_writing_in_dart.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
